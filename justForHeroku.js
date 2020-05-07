697b995bd8b061062fb5f145d964eff12db8bb03
vn50fsa <Rodolfo.Saldivar.Hernandez@walmart.com>
2020-05-07 11:56:22 -0500

    cors

--- justForHeroku.js
+++ justForHeroku.js
@@ -2,6 +2,13 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 const port = process.env.PORT || '5000';
