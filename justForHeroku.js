4fdf3231e6c6b7eafbb06ec19a6dad8aa5bbf33a
vn50fsa <Rodolfo.Saldivar.Hernandez@walmart.com>
2020-05-07 11:21:55 -0500

    heroku json

--- justForHeroku.js
+++ justForHeroku.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-app.use('/static', express.static(path.join(__dirname, 'build/static')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 const port = process.env.PORT || '5000';
 app.listen(port, () => console.log(`PORT: ${port}`));
