69a0bfbdacf984e0690a73322290b80fc2f3ddd9
vn50fsa <Rodolfo.Saldivar.Hernandez@walmart.com>
2020-05-07 11:08:52 -0500

    Update justForHeroku.js

--- justForHeroku.js
+++ justForHeroku.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-app.use('/', express.static(path.join(__dirname, 'build')));
+app.use('/static', express.static(path.join(__dirname, 'build/static')));
 
 const port = process.env.PORT || '5000';
 app.listen(port, () => console.log(`PORT: ${port}`));
